c0ca35ba268c34fa99a696762f1a5d251097d501
henok <henokmicrometre@gmail.com>
2021-04-08 17:10:52 +0100

    removed cv

--- files/.conky-start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sleep 5
-conky -q -c /home/ubuntu/.conky/concky1/conckyrc1 &
-conky -q -c /home/ubuntu/.conky/concky2/conckyrc2 & exit
--- scripts/vim-ide.sh
+++ scripts/vim-ide.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 #install dpendencies
-cd ~/
-curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh -o install_nvm.sh
-bash install_nvm.sh
-source ~/.profile
-nvm ls-remote
-nvm install 12.16.2
-nvm use 12.16.2
 node -v
 nvm ls
 #install vim plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cd ~/
-#set neovim
-#mkdir -p .config/nvim
-#ln -s ~/.vimrc ~/.config/nvim/init.vim
-#download configs 
 wget https://raw.githubusercontent.com/micrometre/dotfiles/master/files/.vimrc
 wget https://raw.githubusercontent.com/micrometre/dotfiles/master/files/.tmux.conf
 wget https://raw.githubusercontent.com/micrometre/dotfiles/master/files/.tern-project
