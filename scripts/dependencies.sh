10b2e63335cc0d7d0464a6246ed913aad5ae2cdd
henok <henokmicrometre@gmail.com>
2021-04-08 23:44:20 +0100

    starting pdf

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -4,10 +4,6 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt upgrade -y && \
 sudo apt install -y \
 git \
-grip \
-vprerex \
-latexmk \
-#vim-gtk # temp solution for enable +python feature in vim
 build-essential \
 make \
 software-properties-common \
@@ -16,7 +12,6 @@ curl \
 wget \
 openssh-server \
 bash-completion \
-vim \
 openssh-server \
 tmux \
 apt-transport-https \
@@ -24,5 +19,16 @@ ca-certificates \
 aptitude \
 python3 \
 python3-pip \
-python3-venv 
+python3-venv \
+vim \
+grip \
+vprerex \
+latexmk \
+texlive-lang-english \
+texlive-base \
+texlive-latex-base \
+texlive-latex-extra \
+vim-gtk \
+texmaker 
+#vim-gtk # temp solution for enable +python feature in vim
 sudo apt autoremove -y
