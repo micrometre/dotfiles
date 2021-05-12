ce909a05579eace18a9eb185cfe6b2e54bd22ea2
henok <henokmicrometre@gmail.com>
2021-05-12 09:16:57 +0100

    sorting scripts/install_kvm.sh

--- scripts/install_kvm.sh
+++ scripts/install_kvm.sh
@@ -8,7 +8,7 @@ sudo apt install -y \
 qemu-kvm \
 libvirt-daemon-system \
 libvirt-clients \
-bridge-utils
+bridge-utils \
+virt-viewr
 sudo adduser `id -un` libvirt
-sudo adduser `id -un` libvirtd
 echo "Installation complete, you need to relogin to run virtual machines."
